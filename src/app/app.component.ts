fd03c17d92f4a253dc02645e9f18b29de4f3cb5e
Silas Owu <owusilasyp00@gmail.com>
2024-07-29 01:21:55 -0700

    style: create storyteller ui with assets added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { HomepageComponent } from './views/homepage/homepage.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomepageComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'story-telling';
+  title = 'interactive-storyteller';
 }
